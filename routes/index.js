fd3b59ce49dd150d311b8196247d70ab5de3c30a
moon-kun-woong <plm303333@gmail.com>
2024-06-10 13:55:14 +0900

    기존의 callback 코드와 promise 코드 둘다 작성.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var router = express.Router();
 const dbConnect = require('../routes/repository');
+const db = require('mysql2');
 const bodyParser = require('body-parser');
 const { error } = require('console');
 
 router.use(express.json());
 router.use(bodyParser.json());
+
+
+
+const connection = db.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '6361427l??',
+  database: 'tingting_db'
+})
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err
+  console.log('The solution is: ', rows[0].solution)
+})
 
 
 // 미들웨어
@@ -43,6 +59,58 @@ router.get('/', function(req, res, next) {
 });
 
 
+
+
+
+// 처음에 작성한 코드
+router.get('/call/cats', (req, res, next) => {
+  console.log("-check request-");
+  connection.query('SELECT * FROM `cats`', (err, results,fields) => {
+    if (err) {
+      console.log(fields);
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({ message: 'success', data: results });
+  })
+});
+
+
+router.post('/call/cats', (req, res, next) => {
+  const { name, age, breed } = req.body;
+  console.log("Received data:", { name, age, breed });
+  connection.query('INSERT INTO cats (name, age, breed) VALUES (?, ?, ?)', [name, age, breed], (err, result) => {
+    if (err) {
+        console.log(err);
+        res.status(500).json({ error: err.message });
+        return;
+    }
+    res.json({ message: 'success', data: { id: result.insertId, name, age, breed } })
+  });
+});
+
+
+router.delete('/call/cats/:id', (req, res) => {
+  const { id } = req.params;
+
+  if(!id){
+    console.log("id null");
+    res.status(400).json({error:'id is empty'});
+    return;
+  }
+  connection.query('DELETE FROM cats WHERE id = ?' , [id], (err,result) =>{
+    if(err){
+      console.error("500err" , err);
+      res.status(500).json({error:"500err"});
+      return;
+    }
+    res.json({ message: 'success', data: result });
+  })
+});
+
+
+
+// async await 은 항상 프로미스를 반환. 아래 API 는 프로미스가 반환되게 짜낸 코드임.
 router.get('/cats/:id', async (req, res) =>{
   const { id } = req.params;
   console.log("-check request- \n id : " , id);
